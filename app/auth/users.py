d70970eb7cf080f4baffac3aa316bee7644d96a6
Akash <nobody8972@gmail.com>
2025-09-09 19:48:34 +0530

    Base Commit

--- app/auth/users.py
+++ app/auth/users.py
@@ -44,9 +44,6 @@ def create_user_token(*, user_id: UUID, expires_delta: timedelta | None = None):
 def get_user_by_id(db:Session, user_id: UUID):
     return db.query(User).filter(User.id == user_id).first()
 
-def get_user_by_username(db:Session,username:str):
-    return db.query(User).filter(User.username==username).first()
-
 def authenticate_user(db: Session, user_data: UserLogin):
     user = (
         db.query(User)
