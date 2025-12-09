cc6e80337f24fd8df3a6bdf645e901aa87349517
go2akash <nobody8972@email.com>
2025-12-09 12:19:28 +0530

    containerized whole prjoect

--- app/db/base.py
+++ app/db/base.py
@@ -8,9 +8,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in environment variables")
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
-)
+engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 from .accounts import Account
 from .transactions import BankTransaction
 from .users import User
+from .admin import Admin
 
 # Optionally, you can also do:
-__all__ = ["Account", "BankTransaction", "User"]
+__all__ = ["Account", "BankTransaction", "User", "Admin"]
